f807d7e4f96540d384adef631721cdb4f583e9ce
Brett Whitford <whitford.15@osu.edu>
2017-04-01 14:45:29 -0400

    Data Visualizatoin: data read in

--- data_visualization.m
+++ data_visualization.m
@@ -1,2 +1,7 @@
 %% Read in data from files
-A = load('datasets/DowJones');
+DowJones = load('datasets/DowJones.mat');
+FF49Industries = load('datasets/FF49Industries.mat');
+FTSE100 = load('datasets/FTSE100.mat');
+NASDAQ100 = load('datasets/NASDAQ100.mat');
+NASDAQComp = load('datasets/NASDAQComp.mat');
+SP500 = load('datasets/SP500.mat');
