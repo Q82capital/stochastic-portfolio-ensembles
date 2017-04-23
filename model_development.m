c90b3e57cded3931e88c3756d82b7b67f075d68a
Brett Whitford <whitford.15@osu.edu>
2017-04-23 14:25:52 -0400

    Share Ratio implemented

--- model_development.m
+++ model_development.m
@@ -3,3 +3,34 @@ clc;
 clear all;
 close all;
 
+load('Data/IndexReturns.mat'); 
+load('Data/ModelReturns.mat'); 
+
+% Extract Constants
+nWeeks = size(ModelReturns, 1);
+nModels = size(ModelReturns, 2);
+windowSize = 12;
+nYears = 12;
+nWindows = nWeeks/windowSize;
+
+%% Calculate Scoring Predictors
+holisticIndexYearlyReturn = nthroot(prod(IndexReturns + 1), nYears);
+holisticModelYearlyReturn = zeros(1, nModels);
+ for i = 1:nModels
+    holisticModelYearlyReturn(1, i) = nthroot(prod(ModelReturns(:,i) + 1), nYears);
+ end
+ 
+%% Calculate Sharpe 
+
+% Assumed constant 3% risk-free return on cash (see [Bridgewater] )
+weeklyCashReturn = nthroot(1.03, 52) - 1;
+RiskFreeReturns = ones(nWeeks, 1) * weeklyCashReturn;
+
+for i = 1:nWindows
+    startWeek = (i-1) * windowSize + 1;
+    endWeek = startWeek + 11;
+    windowSharpe(i, :) = sharpe(ModelReturns(startWeek:endWeek, :), RiskFreeReturns(startWeek:endWeek, :));
+end
+
+
+ 
