6942a4024091e13f5b0ef3ffac0a81303ff0fa53
Brett Whitford <whitford.15@osu.edu>
2017-04-24 23:52:19 -0400

    New response variable finished

--- calc_response.m
+++ calc_response.m
@@ -1,12 +1,34 @@
-function [response] = calc_response(windowAlpha, nWindows, nModels)
+function [response, targetHits] = calc_response(windowAlpha, targetAlpha, nWindows, nModels)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
+% Find which models hit the alpha target
+targetHits = zeros(nWindows-1, nModels);
 response = zeros(nWindows-1, nModels);
 
 for i = 1:nWindows-1
-    response(i, :) = windowAlpha(i+1,:);
+    for j = 1:nModels
+        if windowAlpha(i,j) > targetAlpha
+            targetHits(i,j) = 1;
+        else
+            targetHits(i,j) = 0;
+        end
+    end
+    
+    % Don't modify target hits (will be returned and used later)
+    response(i,:) = targetHits(i,:);
+    
+    % Make sure at least one model is selected
+    if sum(response(i,:)) == 0
+        [~, maxIndex] = max(windowAlpha(i,:));
+        response(i, maxIndex) = 1;
+    end
+    
+    % Normalize to one (for propabilistic interpretation)
+    response(i,:) = response(i,:)/sum(response(i,:));
+        
 end
 
+    
 end
 
--- model_master_script.m
+++ model_master_script.m
@@ -1,7 +1,6 @@
 %% Read in the data and set constants
 clc;
 clear all;
-close all;
 
 % Load Data
 load('Data/IndexReturns.mat'); 
@@ -23,8 +22,8 @@ nWindows = nWeeks/windowSize;
 %% Calculate the reponse variable
 
 % Set the minimum alpha per WEEK you would like to train the model on
-weekMinAlpha = .01;
-response = calc_response(windowAlpha, nWindows, nModels);
+targetAlpha = .0035;
+response = calc_response(windowAlpha, targetAlpha, nWindows, nModels);
 
 %% Divide into train and test
 
@@ -33,8 +32,8 @@ allData = [features, response];
 
 % Set training and test start weeks
 trainStartWindow = 1;
-trainEndWindow = 25;
-testStartWindow = 26;
+trainEndWindow = 30;
+testStartWindow = 31;
 testEndWindow = 44;
 
 % Split the data
@@ -45,8 +44,9 @@ testData = allData (testStartWindow:testEndWindow, :);
 testPred = testData(:, 1:30);
 testResp = testData(:, 31:end);
 
-%% Train neural network
+for test = 1: 100
 
+%% Train neural network
 nHiddenNodes = 50;
 validationFrac = .25;
 maxEpochs = 100;
@@ -63,25 +63,56 @@ end
 plotIsOn = 1; 
 allReturns = calc_returns(ModelReturns, IndexReturns, bestModelIndex, testStartWindow, windowSize, nWeeks, plotIsOn);
 
-%% Print the performance of our model, other models, and index performance
+%% Calculate model risks
+modelRisks = zeros(1, nModels + 1);
+for i = 1:nModels
+    modelRisks(i) = std(ModelReturns(:, i));
+end
+modelRisks(nModels + 1) = std(IndexReturns);
+
+% Calculate our risk by averaging risk of models we chose
+ourRisk = 0;
+for i = 1:length(bestModelIndex)
+    ourRisk = ourRisk + modelRisks(bestModelIndex(i));
+end
+ourRisk = ourRisk/length(bestModelIndex);
+
+%% Calculate model yearly returns
 
 % Calculate the number of years we tested over
 nTestYears = (testEndWindow - testStartWindow)/4;
 
-% Model vs. Index performance
-fprintf('Our models return was %.4f per year \n', nthroot(allReturns(8), nTestYears) - 1);
-fprintf('Index return was %.4f per year \n', nthroot(allReturns(7), nTestYears) - 1);
+yearlyReturns = zeros(1, nModels + 1);
+for i = 1:nModels + 1
+    yearlyReturns(i) = nthroot(allReturns(i), nTestYears) - 1;
+end
+ourReturn = nthroot(allReturns(nModels + 2), nTestYears) - 1;
+
+%% Print the performance of our model, other models, and index performance
+
+% Model performance
+fprintf('Our     return was %.4f per year with risk %.4f \n', ourReturn, ourRisk);
 
 % Comparison to all other strategies
-fprintf('CZeSD return was %.4f per year \n', nthroot(allReturns(1), nTestYears) - 1);
-fprintf('KP_SSD return was %.4f per year \n', nthroot(allReturns(2), nTestYears) - 1);
-fprintf('L_SSD return was %.4f per year \n', nthroot(allReturns(3), nTestYears) - 1);
-fprintf('LR_ASSD return was %.4f per year \n', nthroot(allReturns(4), nTestYears) - 1);
-fprintf('MeanVar return was %.4f per year \n', nthroot(allReturns(5), nTestYears) - 1);
-fprintf('RMZ_SSD return was %.4f per year \n', nthroot(allReturns(6), nTestYears) - 1);
+fprintf('CZeSD   return was %.4f per year with risk %.4f \n', yearlyReturns(1), modelRisks(1));
+fprintf('KP_SSD  return was %.4f per year with risk %.4f \n', yearlyReturns(2), modelRisks(2));
+fprintf('L_SSD   return was %.4f per year with risk %.4f \n', yearlyReturns(3), modelRisks(3));
+fprintf('LR_ASSD return was %.4f per year with risk %.4f \n', yearlyReturns(4), modelRisks(4));
+fprintf('MeanVar return was %.4f per year with risk %.4f \n', yearlyReturns(5), modelRisks(5));
+fprintf('RMZ_SSD return was %.4f per year with risk %.4f \n', yearlyReturns(6), modelRisks(6));
+fprintf('Index   return was %.4f per year with risk %.4f \n', yearlyReturns(7), modelRisks(7));
 
-disp(bestModelIndex);
-
-
-
-
+% disp(bestModelIndex);
+if ishandle(1)
+    clf(1);
+end
+figure(1);
+hold on 
+scatter(modelRisks, yearlyReturns, 'g*');
+lsline
+scatter(ourRisk, ourReturn, 'r*');
+xlabel('Risk');
+ylabel('Return');
+hold off 
+title(test);
+end
